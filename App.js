cbc9928325ed89ca26a696fe55fa383333fbc5c8
fraval23 <136550356+fraval23@users.noreply.github.com>
2023-07-05 22:20:56 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Idcards from './Idcards';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Welcome to IDCards App!</h1>
+ {/* <Idcard/> */}
+ <Idcards/>
+        
+    </div>
+  );
+}
+
+export default App;
